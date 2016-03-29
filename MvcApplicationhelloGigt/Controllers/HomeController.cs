35d9a18db1fe61826531e15afc4a23349cb24ae9
Yudi Goldgrab <ygoldgrab@gmail.com>
2016-03-28 21:52:57 -0400

    added branch

--- MvcApplicationhelloGigt/Controllers/HomeController.cs
+++ MvcApplicationhelloGigt/Controllers/HomeController.cs
@@ -36,5 +36,10 @@ namespace MvcApplicationhelloGigt.Controllers
             Response.Write("<h1>hello foo</h1>");
             return View();
         }
+        public ActionResult ()branch
+        {
+            Response.Write("<h1>hello foo</h1>");
+            return View();
+        }
     }
 }
